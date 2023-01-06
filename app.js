f6362bfee7c8aa86f521da7c73ab22e0c651af66
AbhaySingh <tan63453@gmail.com>
2023-01-06 15:54:25 +0530

    calc

---
+++ app.js
@@ -0,0 +1,20 @@
+const buttons = document.querySelectorAll('button');
+const screen = document.getElementById('screen');
+for (let button of buttons) {
+    button.addEventListener('click', (e) => {
+        const buttontext = e.target.innerText;
+        if (buttontext === 'C') screen.value = "";
+        else if (buttontext === 'x') screen.value += "*";
+        else if (buttontext === '=') {
+            try {
+                screen.value = eval(screen.value);
+            }
+            catch (e) {
+                screen.value = 'invalid';
+            }
+        }
+        else {
+            screen.value += buttontext;
+        }
+    })
+}
